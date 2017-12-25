5040bf70d5462058c4401029cc58d1bb7f75a2de
Usman Ahmad <usman.ahmad@gosign.de>
2017-12-25 21:08:46 +0500

    adds service worked

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function(event) {
+    console.log('service worked installed');
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('service worked installed');
+});
